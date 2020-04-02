e0b6f7d4bac6882fc3545524f5928c07fff15f41
syahaina20 <syahaina20@gmail.com>
2020-04-02 12:18:01 +0800

    Updates

---
+++ precache-manifest.7b8b8a5e970a09d04970e5c5801dcde6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7af8ad3e969443c776462414fd0d94b",
+    "url": "/React-in-github-simple-vote/index.html"
+  },
+  {
+    "revision": "dead436eed964547b9a3",
+    "url": "/React-in-github-simple-vote/static/css/main.ff90ef26.chunk.css"
+  },
+  {
+    "revision": "ec58ca1e295fccc0650b",
+    "url": "/React-in-github-simple-vote/static/js/2.e2f1d78e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-simple-vote/static/js/2.e2f1d78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dead436eed964547b9a3",
+    "url": "/React-in-github-simple-vote/static/js/main.1daa1f72.chunk.js"
+  },
+  {
+    "revision": "1eba7cd192728ca0fe52",
+    "url": "/React-in-github-simple-vote/static/js/runtime-main.9ee65790.js"
+  }
+]);
